755d413fcac9e6ff0e973b15cd0249598d1a7d94
stijn.tratsaert <stijn.tratsaert@student.howest.be>
2021-01-06 19:52:46 +0000

    reworked main entrypoint

--- main.cpp
+++ main.cpp
@@ -1,35 +1,62 @@
-
-#include "light-sensor.h"
-#include "movement-sensor.h"
-#include "camera.h"
+#include "main.h"
 
 int main(void)
 {
-    int requestToRecordTimerCount = 1;
+    int requestToRecordTimerCount = 0;
+    int movementCount = 0;
+    int gasCount = 0;
+    int lightCount = 0;
+
+    wiringPiSetup();
 
     LightSensor lightSensor(2591);
     MovementSensor movementSensor(4);
     Camera camera;
+    GasSensor gasSensor(0);
 
     while (true) 
     {
-        // LightData data = lightSensor.getLightData();
-        // bool isDetecting = movementSensor.isDetecting();
+        LightData light = lightSensor.getLightData();
+        bool isDetecting = movementSensor.isDetecting();
 
-        // if (isDetecting || data.visible > 1000) 
-        // {
-        //     camera.recordAndSave();
-        // }
-
-        if (camera.requestToRecord() && requestToRecordTimerCount == 10)
+        bool firstCondition = movementCount >= 5 || lightCount >= 5 || gasCount >= 5;
+        bool secondCondition = camera.requestToRecord() && requestToRecordTimerCount == 10;
+        if (firstCondition || secondCondition)
         {
+            printTrigger(movementCount, gasCount, lightCount);
             camera.recordAndSave();
-            requestToRecordTimerCount = 1;
+            requestToRecordTimerCount = 0;
+            movementCount = 0;
+            lightCount = 0;
+            gasCount = 0;
+            continue;
         }
 
-        delay(500);
+        if (isDetecting) movementCount++;
+        else movementCount = 0;
+
+        if (!gasSensor.get()) gasCount++;
+        else gasCount = 0;
+
+        if (light.visible > 150) lightCount++;
+        else lightCount = 0;
+
+        delay(1000);
         requestToRecordTimerCount++;
     }
 
     return 0;
+}
+
+void printTrigger(int movementCounter, int gasCounter, int lightCounter)
+{
+    if (movementCounter >= 5) 
+        std::cout << "Camera triggered by movement.\n";
+        return;
+    if (gasCounter >= 5)
+        std::cout << "Camera triggered by gas.\n";
+        return;
+    if (lightCounter >= 5)
+        std::cout << "Camera triggered by light.\n";
+        return;
 }
---
+++ main.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "light-sensor.h"
+#include "movement-sensor.h"
+#include "camera.h"
+#include "gas-sensor.h"
+
+int main();
+void printTrigger(int movementCounter, int gasCounter, int lightCounter);
